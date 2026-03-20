46ac7d08126a53f536743a8f3c872c57b679f123
kalpavruksha G <154514800+Kalpavruksha@users.noreply.github.com>
2026-03-20 12:10:52 +0530

    Update route.ts

--- app/api/careers/route.ts
+++ app/api/careers/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
 import nodemailer from 'nodemailer'
-
 export async function POST(request: NextRequest) {
   try {
     const formData = await request.formData()
